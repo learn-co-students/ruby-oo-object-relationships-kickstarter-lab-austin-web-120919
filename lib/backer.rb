6166372b7a65367c5cf2b2389a1c7e5d12825b5c
github username <github email address>
2020-01-22 15:44:31 -0600

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+class Backer
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project,self)
+    end
+
+    def backed_projects
+        projects = ProjectBacker.all.select do |pb| 
+            pb.backer == self
+        end
+        projects.map(&:project)
+    end
+
+end
